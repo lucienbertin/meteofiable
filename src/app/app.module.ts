eb6ea51a676fd0580544aec017b904133d26d783
lucienbertin <me@lucienbertin.com>
2018-04-13 16:27:47 +0200

    temp debug code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule
+	],
+	providers: [
+		
+	],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
