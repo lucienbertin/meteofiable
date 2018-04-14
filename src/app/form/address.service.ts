41fb82f8b622318ef486d73d7bd53fbb8611a35d
lucienbertin <me@lucienbertin.com>
2018-04-14 14:09:02 +0200

    add typing in http get

--- src/app/form/address.service.ts
+++ src/app/form/address.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 // import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/map';
+import { IGmapApiResponse } from 'app/common';
 
 @Injectable()
 export class AddressService {
@@ -13,10 +14,9 @@ export class AddressService {
 
 	searchAddresses(clue = '') {
 		const transformedClue = clue.replace(' ', '+')
-		return this.http.get(`${this.gmapApi}?address=${transformedClue}`)
-		.map((response: any) => {
+		return this.http.get<IGmapApiResponse>(`${this.gmapApi}?address=${transformedClue}`)
+		.map(response => {
 			return response.results.map(r => r.formatted_address);
 		});
-		// return Observable.of([clue + ' lol']);
 	}
 }
