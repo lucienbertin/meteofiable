cb0896efdfdf159ca865b83a9c481312f6ddafff
lucienbertin <me@lucienbertin.com>
2018-04-14 13:48:27 +0200

    add datepicker in form

--- src/app/form/components/form.component.ts
+++ src/app/form/components/form.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { AddressService } from '../address.service';
 import { Observable } from 'rxjs/Observable';
+import { MatDatepicker } from '@angular/material';
+import 'rxjs/add/operator/delay';
 
 @Component({
 	selector: 'mf-form',
@@ -11,7 +13,9 @@ import { Observable } from 'rxjs/Observable';
 export class MfFormComponent {
 	model = {};
 	adresses = [];
-
+	@ViewChild('addressInput') addressInput: ElementRef;
+	@ViewChild('dateInput') dateInput: ElementRef;
+	
 	constructor(
 		private addressService: AddressService,
 	) {}
@@ -21,5 +25,47 @@ export class MfFormComponent {
 	searchAddresses(clue = '') {
 		this.addressService.searchAddresses(clue)
 		.subscribe(results => this.adresses = results);
+	}
+
+	openPicker(picker: MatDatepicker<any>) {
+		if (!picker.opened) {
+			picker.open();
+			this.refocusInput();
+		}
+	}
+	closePicker(picker: MatDatepicker<any>) {
+		if (picker.opened) {
+			picker.close();
+		}
+	}
+	togglePicker(picker: MatDatepicker<any>) {
+		if (!picker.opened) {
+			picker.open();
+			this.refocusInput();
+		} else {
+			picker.close();
+		}
+	}
+	onTab(picker: MatDatepicker<any>) {
+		picker.close();
+	}
+	onEnter(picker: MatDatepicker<any>) {
+		picker.close();
+	}
+	onEscape(picker: MatDatepicker<any>) {
+		picker.close();
+	}
+	private refocusInput() {
+		Observable.of({})
+			.delay(1) // need timeout here because shenanigans
+			.subscribe(() => {
+				this.dateInput.nativeElement.focus();
+			});
+	}
+	focus(picker: MatDatepicker<any>) {
+		this.openPicker(picker);
+	}
+	blur() {
+		// this.to._isFocused = false;
 	}
 }
