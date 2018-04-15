cca88c19b12fa1a40177a9410be9a5ecc8b560f6
lucienbertin <me@lucienbertin.com>
2018-04-15 18:07:16 +0200

    init ngrx for app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 import { MatAutocompleteModule, MatOptionModule } from '@angular/material';
 import { AppComponent } from './app.component';
 import { MfFormModule } from './form';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
 	declarations: [
@@ -11,6 +14,10 @@ import { MfFormModule } from './form';
 	imports: [
 		BrowserModule,
 		MfFormModule,
+
+		StoreModule.forRoot({}),
+		EffectsModule.forRoot([]),
+		StoreDevtoolsModule.instrument({}),
 	],
 	providers: [
 	],
