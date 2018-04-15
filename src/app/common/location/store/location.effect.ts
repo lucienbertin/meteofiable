342659f9f4a2cf3ddd1ea948365a585ad26bfa49
lucienbertin <me@lucienbertin.com>
2018-04-15 21:55:07 +0200

    use complete and follow in location effects

--- src/app/common/location/store/location.effect.ts
+++ src/app/common/location/store/location.effect.ts
@@ -2,7 +2,7 @@
 import 'rxjs/add/operator/map';
 import { Injectable } from '@angular/core';
 import { Effect } from '@ngrx/effects';
-import { MfActions, DoNothingCommand, ARequest } from 'app/core';
+import { MfActions, DoNothingCommand, ARequest, AAction } from 'app/core';
 
 import { SetAdressCmd } from './location.command';
 import { SetGeocodeEvt } from './location.event';
@@ -14,18 +14,29 @@ class AddressToGeocodeRequest extends ARequest<IGmapGeocode> {
 		super('[req] gmap - address to geocode');
 	}
 }
-
+class DebugAction extends AAction<number> {
+	constructor(a: AAction) {
+		super(
+			`[debug] '${a.type}' correlated to command of id: ${a.commandId}`,
+		);
+	}
+}
 @Injectable()
 export class LocationEffect {
-	@Effect() setAddressHandler = this.actions$.of(new SetAdressCmd())
-	.map(c =>
-		new AddressToGeocodeRequest()
-		.follow(c)
-		.setCall(() => this.gmapService.searchAddresses(c.payload.address).map(geocodes => geocodes[0]))
-	);
-	@Effect() callAddressApi = this.actions$.call(new AddressToGeocodeRequest());
-	@Effect() gotGeocodeFromAddress = this.actions$.success(new AddressToGeocodeRequest())
-	.map(r => new SetGeocodeEvt(r.data).follow(r));
+	@Effect() setAddressHandler = this.actions$
+		.of(new SetAdressCmd())
+		.follow(c =>
+			new AddressToGeocodeRequest()
+			.setCall(() => this.gmapService.searchAddresses(c.payload.address).map(geocodes => geocodes[0]))
+		);
+	@Effect() callAddressApi = this.actions$
+		.call(new AddressToGeocodeRequest());
+	@Effect() gotGeocodeFromAddress = this.actions$
+		.success(new AddressToGeocodeRequest())
+		.follow(r => new SetGeocodeEvt(r.data));
+	@Effect() setAddressComplete = this.actions$
+		.complete(new SetAdressCmd(), new SetGeocodeEvt());
+
 	constructor(
 		private actions$: MfActions,
 		private gmapService: GmapService,
