462d67da7deec2ba3644186bae7916a29c6716d7
lucienbertin <me@lucienbertin.com>
2018-04-18 11:46:45 +0200

    use said randomizer for forecast temperature with seedkey from placeid + date

--- src/app/models/forecast.model.ts
+++ src/app/models/forecast.model.ts
@@ -1,6 +1,8 @@
 import { IGmapGeocode } from './gmap.model';
 import { Moment } from 'moment';
 
+import { Randomizer } from './randomizer';
+
 export interface IForecast {
 	geocode: IGmapGeocode;
 	date: Moment;
@@ -21,6 +23,10 @@ export class Forecast implements IForecast {
 			this.city = !!cityComponent ? cityComponent.long_name : '';
 			const countryComponent = this.geocode.address_components.find(ac => !!ac.types.find(t => t === 'country'));
 			this.country = !!countryComponent ? countryComponent.long_name : '';
+
+			const seedKey = `${this.geocode.place_id}${this.date.toISOString()}`;
+			const random = new Randomizer(seedKey);
+			this.temperature = Math.floor(20 + 7 * random.next());
 		}
 }
 export enum Weather {
