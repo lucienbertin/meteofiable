9c0f6e7f733b59772c7e219e4e083b2416069046
lucienbertin <me@lucienbertin.com>
2018-04-18 17:56:09 +0200

    misc

--- src/app/models/forecast.model.ts
+++ src/app/models/forecast.model.ts
@@ -18,16 +18,17 @@ export class Forecast implements IForecast {
 		public geocode: IGmapGeocode,
 		public date: Moment,
 		public weather = Weather.sunny,
-		public temperature = 25) {
-			const cityComponent = this.geocode.address_components.find(ac => !!ac.types.find(t => t === 'locality'));
-			this.city = !!cityComponent ? cityComponent.long_name : '';
-			const countryComponent = this.geocode.address_components.find(ac => !!ac.types.find(t => t === 'country'));
-			this.country = !!countryComponent ? countryComponent.long_name : '';
+		public temperature = 25,
+	) {
+		const cityComponent = this.geocode.address_components.find(ac => !!ac.types.find(t => t === 'locality'));
+		this.city = !!cityComponent ? cityComponent.long_name : '';
+		const countryComponent = this.geocode.address_components.find(ac => !!ac.types.find(t => t === 'country'));
+		this.country = !!countryComponent ? countryComponent.long_name : '';
 
-			const seedKey = `${this.geocode.place_id}${this.date.toISOString()}`;
-			const random = new Randomizer(seedKey);
-			this.temperature = Math.floor(20 + 7 * random.next());
-		}
+		const seedKey = `${this.geocode.place_id}${this.date.toISOString()}`;
+		const random = new Randomizer(seedKey);
+		this.temperature = Math.floor(20 + 7 * random.next());
+	}
 }
 export enum Weather {
 	sunny = 'sunny',
