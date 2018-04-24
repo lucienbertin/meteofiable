9a3c4ad30503a10c7c755c1c799dd126a8400bbc
lucienbertin <me@lucienbertin.com>
2018-04-24 10:26:01 +0200

    browseranimation moduel

--- src/app/form/form.module.ts
+++ src/app/form/form.module.ts
@@ -4,7 +4,7 @@ import { MatMomentDateModule } from '@angular/material-moment-adapter';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LocationStoreModule, GmapModule, DateStoreModule } from 'app/common';
 
 import {
@@ -29,7 +29,7 @@ import {
 		LocationStoreModule,
 		DateStoreModule,
 		GmapModule,
-		NoopAnimationsModule,
+		BrowserAnimationsModule,
 	],
 	providers: [
 	],
